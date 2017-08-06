4cd90245e27d172d2073c17ae3dbf22c5ad0b8d3
rlemm-juniper <rlemm@juniper.net>
2017-08-05 21:10:34 -0500

    initial add

---
+++ convert_gpb_to_json.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+import requests
+import json
+import time
+import datetime
+import os
+import glob
+import logging
+import socket
+import sys
+from json import *
+from telemetry_top_pb2 import *
+from cpu_memory_utilization_pb2 import *
+from firewall_pb2 import *
+from inline_jflow_pb2 import *
+from logical_port_pb2 import *
+from lsp_mon_pb2 import *
+from lsp_stats_pb2 import *
+from npu_memory_utilization_pb2 import *
+from npu_utilization_pb2 import *
+from optics_pb2 import *
+from packet_stats_pb2 import *
+from port_pb2 import *
+from google.protobuf.json_format import *
+from protobuf_to_dict import protobuf_to_dict, TYPE_CALLABLE_MAP
+# try:
+#     import http.client as http_client
+# except ImportError:
+#     import httplib as http_client
+#
+# http_client.HTTPConnection.debuglevel = 1
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+
+#Logical Port
+Logical_Port = LogicalPort()
+Logical_Interface_Info = LogicalInterfaceInfo()
+Ingress_Interface_Stats = IngressInterfaceStats()
+Egress_Interface_Stats = EgressInterfaceStats()
+Operational_State = OperationalState()
+Forwarding_Class_Accounting = ForwardingClassAccounting()
+logical_Interface_Queue_Stats = logicalInterfaceQueueStats()
+
+#Telemetry Top
+Telemetry_Field_Options = TelemetryFieldOptions()
+Telemetry_Stream = TelemetryStream()
+IETF_Sensors = IETFSensors()
+Enterprise_Sensors = EnterpriseSensors()
+Juniper_Networks_Sensors = JuniperNetworksSensors()
+
+#Packet Stats
+Packet_Statistics = PacketStatistics()
+Packet_Stats_Packet_ForwardingEngine = PacketStatsPacketForwardingEngine()
+Packet_Stats_Class = PacketStatsClass()
+Packet_Stats_Counter = PacketStatsCounter()
+
+#Port
+Port = Port()
+Interface_Infos = InterfaceInfos()
+Queue_Stats = QueueStats()
+Interface_Stats = InterfaceStats()
+Ingress_Interface_Errors = IngressInterfaceErrors()
+
+#Optics
+Optics = Optics()
+Optics_Infos = OpticsInfos()
+Optics_Diag_Stats = OpticsDiagStats()
+Optics_DiagLane_Stats = OpticsDiagLaneStats()
+
+#Network Processor Utilization
+Network_Processor_Utilization = NetworkProcessorUtilization()
+Utilization = Utilization()
+Memory_Load = MemoryLoad()
+Packet_Load = PacketLoad()
+
+#Network Processor Memory Utilization
+Network_Processor_Memory_Utilization = NetworkProcessorMemoryUtilization()
+Npu_Memory = NpuMemory()
+Npu_Memory_Summary = NpuMemorySummary()
+Npu_Memory_Partition = NpuMemoryPartition()
+
+#LSP Stats
+Lsp_Stats = LspStats()
+Lsp_Stats_Record = LspStatsRecord()
+
+#LSP Monitoring
+key = key()
+lsp_monitor_data_event = lsp_monitor_data_event()
+ero_type_entry = ero_type_entry()
+ero_ipv4_type = ero_ipv4_type()
+rro_type_entry = rro_type_entry()
+rro_ipv4_type = rro_ipv4_type()
+lsp_monitor_data_property = lsp_monitor_data_property()
+lsp_mon = lsp_mon()
+
+#Inline JFlow Monitoring
+Inline_Jflow = InlineJflow()
+Inline_Jflow_Npu_Stats = InlineJflowNpuStats()
+
+#Firewall
+Firewall = Firewall()
+Firewall_Stats = FirewallStats()
+Memory_Usage = MemoryUsage()
+Counter_Stats = CounterStats()
+Policer_Stats = PolicerStats()
+Extended_Policer_Stats = ExtendedPolicerStats()
+Hierarchical_Policer_Stats = HierarchicalPolicerStats()
+
+#CPU Memory
+Cpu_Memory_Utilization = CpuMemoryUtilization()
+Cpu_Memory_Utilization_Summary = CpuMemoryUtilizationSummary()
+Cpu_Memory_Utilization_Per_Application = CpuMemoryUtilizationPerApplication()
+
+variables = [Telemetry_Stream]
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 30000
+
+socket = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+socket.bind((UDP_IP, UDP_PORT))
+
+roomKey = ''
+collection_name = ''
+url = 'http://172.25.137.172:8090/version/2.0/post_event'
+HEADERS = {'content-type': 'application/json'}
+
+def post_url(json_data):
+    print json_data
+    requests.post(url=url, data=json_data, headers=HEADERS)
+
+while True:
+    gpb_data, addr = socket.recvfrom(65535) # buffer size is 1024 bytes
+    try:
+        for y in variables:
+            y.ParseFromString(gpb_data)
+            y = json.dumps(protobuf_to_dict(y, type_callable_map=TYPE_CALLABLE_MAP, use_enum_labels=True))
+            y = json.loads(y)
+            roomKey = y['system_id']
+            timestamp = y['timestamp']
+            data = {'collection_name': 'jdi_usage_collection', 'data': {'Timestamp': timestamp, \
+                    'roomKey': roomKey, 'jti_info': y}, 'tailwind_manager': {}}
+            json_data = json.dumps(data).replace("\\", "").replace('""', '"').replace("u'", "'")
+            post_url(json_data)
+    except:
+        e = sys.exc_info()[0]
+        print e
+        pass
+
